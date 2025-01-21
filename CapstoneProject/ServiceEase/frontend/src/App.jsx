3f111ff45bdf20b29e85afa7faa511f293836fa6
Meeracle01 <mmenon40@gmail.com>
2025-01-21 22:25:45 +1300

    missing Capstone documentation & working capstone

--- CapstoneProject/ServiceEase/frontend/src/App.jsx
+++ CapstoneProject/ServiceEase/frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Region from './pages/Region';  // Add the Region component import
-import ServicesList from '../frontend/src/ServicesList'; // Import the ServicesList component
+import ServicesList from './components/ServicesList';
 
 function App() {
   return (
-    <Router>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -16,7 +16,7 @@ function App() {
         <Route path="/region/:regionName" element={<Region />} /> {/* Add Route for Region page */}
         <Route path="/services" element={<ServicesList />} /> {/* Add Route for ServicesList page */}
       </Routes>
-    </Router>
+
   );
 }
 
--- CapstoneProject/ServiceEase/frontend/vite.config.js
+++ CapstoneProject/ServiceEase/frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 5173 // Make sure this matches the port in your terminal output
+  }
+});
